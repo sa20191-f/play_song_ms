a9dfdb088ed98a2897ad9da0d28a248aafd8b053
Nicolás Campuzano <nicolas.campuzano@lantri.com>
2019-05-16 22:25:42 -0500

    Change the controller

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace play_song_ms.Controllers {
+
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SongsController : ControllerBase {
+    
+    // GET api/songs/
+    [HttpGet]
+    [HttpGet("{id}")]
+    public ActionResult<string> Get() {
+      return "success";
+    }
+  }
+}
